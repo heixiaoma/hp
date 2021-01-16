d9bb54a2fa288055ae8210797e6c3676e0c0bec4
黑小马 <heixiaoma@gmail.com>
2021-01-16 16:06:19 +0800

    优化代码,可用端口获取测试

---
+++ hp-server/src/test/java/test/PortTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class PortTest {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null; //读取空闲的可用端口
+        try {
+            serverSocket = new ServerSocket(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(serverSocket.getLocalPort());
+        }
+    }
+}
