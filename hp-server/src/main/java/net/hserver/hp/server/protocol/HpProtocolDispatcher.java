38b40cd03cf1c181cd1321eaa7561ccbc9b73738
hxm <heixiaoma@gmail.com>
2021-11-03 14:16:13 +0800

    优化数据

--- hp-server/src/main/java/net/hserver/hp/server/protocol/HpProtocolDispatcher.java
+++ hp-server/src/main/java/net/hserver/hp/server/protocol/HpProtocolDispatcher.java
@@ -14,17 +14,21 @@ import top.hserver.core.ioc.annotation.Order;
 @Order(6)
 @Bean
 public class HpProtocolDispatcher implements ProtocolDispatcherAdapter {
-
     private HpServerHandler hpServerHandler = new HpServerHandler();
 
-    //000630001000
+    //000
     @Override
     public boolean dispatcher(ChannelHandlerContext channelHandlerContext, ChannelPipeline channelPipeline, byte[] bytes) {
-        if (bytes[0] == 0 && bytes[1] == 0 && bytes[2] == 0 && bytes[3] == 63 && bytes[4] == 0 && bytes[5] == 0 && bytes[6] == 0) {
+        if (bytes[0] == 0 && bytes[1] == 0 && bytes[2] == 0) {
             channelPipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4),
                     new HpMessageDecoder(), new HpMessageEncoder(),
                     new IdleStateHandler(60, 30, 0), hpServerHandler);
             return true;
+        } else {
+            System.out.println("错误数据");
+            for (int i = 0; i < bytes.length; i++) {
+                System.out.print(bytes[i]);
+            }
         }
         return false;
     }
