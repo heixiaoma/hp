0e3aee651297c9d9900b4ac9d156013c7e28b1e0
黑小马 <Mcl135799>
2022-08-12 18:44:03 +0800

    基于golang的客服端

--- hp-common/src/main/proto/gen.bat
+++ hp-common/src/main/proto/gen.bat
@@ -1,2 +1,4 @@
 protoc -I E:\java\hp\hp-common\src\main\proto  --java_out=E:\java\hp\hp-common\src\main\proto E:\java\hp\hp-common\src\main\proto\HpMessage.proto
 protoc --go_out=. HpMessage.proto
+protoc --python_out=. HpMessage.proto
+
