67319a9f494a3d3d69ba312510138b6bd8d3dbe0
hxm <heixiaoma@gmail.com>
2022-08-13 10:19:31 +0800

    proto 编码解码处理

---
+++ hp-client-golang/Protol/Protol.go
@@ -0,0 +1,83 @@
+package Protol
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"github.com/golang/protobuf/proto"
+	"hp-client-golang/HpMessage"
+)
+
+func Encode(message *HpMessage.HpMessage) []byte {
+	d, _ := proto.Marshal(message)
+	i, _ := encode(d)
+	return i
+}
+
+func Decode(reader *bufio.Reader) (*HpMessage.HpMessage, error) {
+	d, err := decode(reader)
+	if err != nil {
+		return nil, err
+	}
+	message := &HpMessage.HpMessage{}
+	proto.Unmarshal(d, message)
+	return message, nil
+}
+
+//将数据包编码（即加上包头再转为二进制）
+func encode(mes []byte) ([]byte, error) {
+	//获取发送数据的长度，并转换为四个字节的长度，即int32
+	len := int8(len(mes))
+	//创建数据包
+	dataPackage := new(bytes.Buffer) //使用字节缓冲区，一步步写入性能更高
+
+	//先向缓冲区写入包头
+	//大小端口诀：大端：尾端在高位，小端：尾端在低位
+	//编码用小端写入，解码也要从小端读取，要保持一致
+	err := binary.Write(dataPackage, binary.LittleEndian, len) //往存储空间小端写入数据
+	if err != nil {
+		return nil, err
+	}
+	//写入消息
+	err = binary.Write(dataPackage, binary.LittleEndian, mes)
+	if err != nil {
+		return nil, err
+	}
+	return dataPackage.Bytes(), nil
+}
+
+//解码数据包
+func decode(reader *bufio.Reader) ([]byte, error) {
+	//读取数据包的长度（从包头获取）
+	lenHeader := 2
+	lenByte, err := reader.Peek(lenHeader) //读取前2个字节的数据
+	if err != nil {
+		return []byte{}, err
+	}
+	//转成Buffer对象,设置为从小端读取
+	buff := bytes.NewBuffer(lenByte)
+	var len uint8                                      //读取的数据大小，初始化为0
+	err = binary.Read(buff, binary.LittleEndian, &len) //从小端读取
+	if err != nil {
+		return []byte{}, err
+	}
+	//读取消息
+	pkg := make([]byte, int(len)+lenHeader)
+	//Buffered返回缓冲区中现有的可读取的字节数
+	if reader.Buffered() < int(len)+lenHeader { //如果读取的包头的数据大小和读取到的不符合
+		hr := 0
+		for hr < int(len)+lenHeader {
+			l, err := reader.Read(pkg[hr:])
+			if err != nil {
+				return []byte{}, err
+			}
+			hr += l
+		}
+	} else {
+		_, err := reader.Read(pkg)
+		if err != nil {
+			return []byte{}, err
+		}
+	}
+	return pkg[lenHeader:], nil
+}
--- hp-client-golang/main.go
+++ hp-client-golang/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
+	"bufio"
 	"fmt"
 	"github.com/golang/protobuf/proto"
 	"hp-client-golang/HpMessage"
+	"hp-client-golang/Protol"
 	"net"
 	"os"
 )
@@ -17,32 +17,10 @@ func ClientHandleError(err error, when string) {
 	}
 }
 
-func IntToBytes(n int) []byte {
-	data := int8(n)
-	bytebuf := bytes.NewBuffer([]byte{})
-	binary.Write(bytebuf, binary.BigEndian, data)
-	return bytebuf.Bytes()
-}
-
-func BytesToInt(bys []byte) int {
-	bytebuff := bytes.NewBuffer(bys)
-	var data int64
-	binary.Read(bytebuff, binary.BigEndian, &data)
-	return int(data)
-}
-
 func main() {
 	//拨号远程地址，简历tcp连接
 	conn, err := net.Dial("tcp", "127.0.0.1:9091")
 	ClientHandleError(err, "client conn error")
-	//HpMessageData.HpMessage.Builder messageBuild = HpMessageData.HpMessage.newBuilder();
-	//messageBuild.setType(HpMessageData.HpMessage.HpMessageType.REGISTER);
-	//HpMessageData.HpMessage.MetaData.Builder metaDataBuild = HpMessageData.HpMessage.MetaData.newBuilder();
-	//metaDataBuild.setPort(port);
-	//metaDataBuild.setUsername(username);
-	//metaDataBuild.setPassword(password);
-	//messageBuild.setMetaData(metaDataBuild.build());
-	//            client.connect("127.0.0.1",9091, "test","123456", 12000, "127.0.0.1", 8888);
 	message := &HpMessage.HpMessage{
 		Type: HpMessage.HpMessage_REGISTER,
 		MetaData: &HpMessage.HpMessage_MetaData{
@@ -51,14 +29,21 @@ func main() {
 			Password: *proto.String("123456"),
 		},
 	}
-	data, _ := proto.Marshal(message)
-	conn.Write(IntToBytes(len(data)))
-	conn.Write(data)
-	//buffer := make([]byte, 1024)
+
+	conn.Write(Protol.Encode(message))
+	readMessage(conn)
+}
+
+//接收消息
+func readMessage(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	//读消息
 	for {
-		//n, _ := conn.Read(buffer)
-		//hpMessage := &HpMessage.HpMessage{}
-		//proto.Unmarshal(buffer[:n], hpMessage)
-		//print(hpMessage.MetaData.Reason)
+		decode, err := Protol.Decode(reader)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(decode)
 	}
 }
