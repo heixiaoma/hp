8ddc7cfb803cbd0996faf69c2b38f927805dd6b5
hxm <heixiaoma@gmail.com>
2022-09-04 10:35:05 +0800

    命令行模式

--- hp-client-golang/main.go
+++ hp-client-golang/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/spf13/viper"
 	"hp-client-golang/tcp"
 	"log"
@@ -9,6 +10,23 @@ import (
 
 func main() {
 
+	// 定义几个变量，用于接收命令行的参数值
+	var username string
+	var password string
+	var remote_port int
+	var ip string
+	var port int
+
+	//命令行参数模式
+	flag.StringVar(&username, "username", "root", "穿透账号")
+	flag.StringVar(&password, "password", "", "穿透密码")
+	flag.IntVar(&remote_port, "remote_port", 0, "外网端口如果没有会随机分配当前是可选参数")
+	flag.StringVar(&ip, "ip", "127.0.0.1", "内网IP")
+	flag.IntVar(&port, "port", 0, "内网端口")
+	// 解析命令行参数写入注册的flag里
+	flag.Parse()
+
+	//配置文件模式
 	config := viper.New()
 	config.AddConfigPath("./")     // 文件所在目录
 	config.SetConfigName("config") // 文件名
@@ -19,13 +37,15 @@ func main() {
 		} else {
 			log.Printf("配置文件出错..")
 		}
+	} else {
+		username = config.GetString("hp.username")
+		password = config.GetString("hp.password")
+		remote_port = config.GetInt("hp.remote_port")
+		ip = config.GetString("proxy.ip")
+		port = config.GetInt("proxy.port")
 	}
-	username := config.GetString("hp.username")
-	password := config.GetString("hp.password")
-	remote_port := config.GetInt("hp.remote_port")
+
 	log.Printf("用户名:%s 密码:%s ,外部访问的端口：%d", username, password, remote_port)
-	ip := config.GetString("proxy.ip")
-	port := config.GetInt("proxy.port")
 	log.Printf("本地IP:%s 本地端口：%d", ip, port)
 	hpClient := tcp.NewHpClient(func(message string) {
 		log.Printf(message)
