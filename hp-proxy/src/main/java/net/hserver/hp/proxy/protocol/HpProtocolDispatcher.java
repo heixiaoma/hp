836abb012f622729f22b95818fc865a92b9c7394
hxm <heixiaoma@gmail.com>
2022-09-12 19:16:15 +0800

    动态端口

--- hp-proxy/src/main/java/net/hserver/hp/proxy/protocol/HpProtocolDispatcher.java
+++ hp-proxy/src/main/java/net/hserver/hp/proxy/protocol/HpProtocolDispatcher.java
@@ -3,6 +3,7 @@ package net.hserver.hp.proxy.protocol;
 import cn.hserver.core.interfaces.ProtocolDispatcherAdapter;
 import cn.hserver.core.ioc.annotation.Bean;
 import cn.hserver.core.ioc.annotation.Order;
+import cn.hserver.core.server.util.PropUtil;
 import cn.hserver.plugin.web.context.WebConstConfig;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
@@ -10,17 +11,23 @@ import io.netty.handler.timeout.IdleStateHandler;
 import net.hserver.hp.common.codec.HpMessageDecoder;
 import net.hserver.hp.common.codec.HpMessageEncoder;
 import net.hserver.hp.proxy.handler.HpServerHandler;
+
 import java.net.InetSocketAddress;
 
 @Order(6)
 @Bean
 public class HpProtocolDispatcher implements ProtocolDispatcherAdapter {
 
+    private Integer port;
+
     //判断HP头
     @Override
     public boolean dispatcher(ChannelHandlerContext ctx, ChannelPipeline channelPipeline, byte[] bytes) {
         InetSocketAddress socketAddress = (InetSocketAddress) ctx.channel().localAddress();
-        if (socketAddress.getPort() == 9091) {
+        if (port == null) {
+            port = PropUtil.getInstance().getInt("port");
+        }
+        if (socketAddress.getPort() == port) {
             channelPipeline.addLast(new IdleStateHandler(60, 30, 0));
             channelPipeline.addLast(new HpMessageDecoder());
             channelPipeline.addLast(new HpMessageEncoder());
