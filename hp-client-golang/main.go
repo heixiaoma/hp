7392a45a048d6dcab2b3719cf067df775cda746e
hxm <heixiaoma@gmail.com>
2022-10-11 22:23:36 +0800

    实现golang web管理

--- hp-client-golang/main.go
+++ hp-client-golang/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"hp-client-golang/web"
+	"log"
 )
 
 func main() {
+	log.Printf("请访问 http://127.0.0.1:10240/ 进行穿透配置")
 	web.StartWeb(0, "http://ksweb.club:9090")
-
 	//
 	//// 定义几个变量，用于接收命令行的参数值
 	//var username string
