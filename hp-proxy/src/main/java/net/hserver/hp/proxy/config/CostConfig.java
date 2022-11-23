548b2d959223f2c7875e30d60acc3c212ae3c765
hxm <heixiaoma@gmail.com>
2022-11-23 19:14:58 +0800

    0

--- hp-proxy/src/main/java/net/hserver/hp/proxy/config/CostConfig.java
+++ hp-proxy/src/main/java/net/hserver/hp/proxy/config/CostConfig.java
@@ -3,5 +3,5 @@ package net.hserver.hp.proxy.config;
 public class CostConfig {
     public static String VER_TOKEN = "";
     public final static int M_H=1024*1024*20;
-    public final static int M_L=1024*1024*10;
+    public final static int M_L=1024*1024*5;
 }
